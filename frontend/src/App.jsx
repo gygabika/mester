2a6eb1bd7c26bee9e6dd9906b27c857ff537fa8a
gygabika <82406669+gygabika@users.noreply.github.com>
2025-03-12 10:35:06 +0100

    asd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import './App.css'
 /*import '../../bootstrap/css/bootstrap.min.css'*/
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
@@ -11,19 +12,17 @@ import { Profilom } from './oldalak/Profilom'
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route element={<Layout/>}>
-            <Route path="/"  element={ <Fooldal/> }/>
-            <Route path="/Autok"  element={ <Autok/> }/>
-            <Route path="/Berleseim"  element={ <Berleseim/> }/>
-            <Route path="/Profilom"  element={ <Profilom/> }/>
-            <Route path="/Hirek"  element={ <Hirek/> }/>
-          </Route>
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route path="/"  element={ <Fooldal/> }/>
+          <Route path="/Autok"  element={ <Autok/> }/>
+          <Route path="/Berleseim"  element={ <Berleseim/> }/>
+          <Route path="/Profilom"  element={ <Profilom/> }/>
+          <Route path="/Hirek"  element={ <Hirek/> }/>
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
