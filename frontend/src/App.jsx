c68dc57b010f98d3fd0ecf0e9b36a669c127e9a8
gygabika <82406669+gygabika@users.noreply.github.com>
2025-03-12 09:57:12 +0100

    bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css'
+/*import '../../bootstrap/css/bootstrap.min.css'*/
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { Layout } from './Layout'
+import { Fooldal } from './oldalak/Fooldal'
+import { Autok } from './oldalak/Autok'
+import { Berleseim } from './oldalak/Berleseim'
+import { Hirek } from './oldalak/Hirek'
+import { Profilom } from './oldalak/Profilom'
 
 
 function App() {
   return (
     <>
-      <div>
-      <h1>Vite + React</h1>
-      </div>
+      <Router>
+        <Routes>
+          <Route element={<Layout/>}>
+            <Route path="/"  element={ <Fooldal/> }/>
+            <Route path="/Autok"  element={ <Autok/> }/>
+            <Route path="/Berleseim"  element={ <Berleseim/> }/>
+            <Route path="/Profilom"  element={ <Profilom/> }/>
+            <Route path="/Hirek"  element={ <Hirek/> }/>
+          </Route>
+        </Routes>
+      </Router>
     </>
   )
 }
--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -5,17 +5,11 @@ import { Logo } from './assets/ceg_logo.png'
 export function Layout() {
     return(
         <>
-            <header>
-                <h1 id="focim">DriveUs</h1>
                 <Navbar/>
-            </header>
             <main>
                 <Outlet/>
             </main>
-            <footer>
-                <img src="./assets/ceg_logo.png" alt="DriveUs Logo" id="footer-img" />  
-                <p id="footer_p">Válogass prémium autóink közül, és indulj útnak stílusosan!</p>
-            </footer>
+            
         </>
     )
 }
